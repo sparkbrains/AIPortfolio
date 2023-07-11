a0ac7ad811ec011fea14ec0bf62ad36ac9d96f3c
sparkbrains <sushil@sparkbrains.in>
2023-07-11 13:23:37 +0530

    updated music convertor and some validation in stock prediction

--- datascience_portfolio/portfolio/views.py
+++ datascience_portfolio/portfolio/views.py
@@ -81,6 +81,7 @@ class ObjectClassification(View):
 
 
 
+
 class MusicGenerationn(View):
     def get(self, request, *args, **kwargs):      
         return render(request,'models_view/music-generation.html')
